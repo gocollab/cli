1f91893db294888c161b904c720c5966f13f862b
Makis Tracend <makis.tracend@gmail.com>
2015-01-28 23:41:41 -0800

    scaffolding

---
+++ examples/setup.sh
@@ -0,0 +1,15 @@
+
+# Installing the GoCollab CLI
+
+# 1. Install the main executable
+npm install -g gocollab-cli
+
+#2. Setup gocollab (only needed once)
+gocollab setup
+
+# you'll be propted with two entries
+# - email: ...
+# - token: ...
+
+# 3. Find out API commands using the help
+gocollab -h
